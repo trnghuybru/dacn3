b209cf376c76271973a28f6ee896ab25d228582a
DANG CONG NHAT <dangcongnhat2004qb@gmail.com>
2026-02-24 17:42:56 +0700

    feat(update): ui home

---
+++ frontend/lib/screens/map_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MapScreen extends StatelessWidget {
+  const MapScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Màn hình Bản đồ',
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
